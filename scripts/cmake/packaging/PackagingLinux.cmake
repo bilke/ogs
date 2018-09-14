8c543b6c4a68a1257b745d392eceb88ee60f34e3
Lars Bilke <lars.bilke@ufz.de>
2018-09-14 12:48:42 +0200

    [CMake] Proper RPATH for installed projects.

--- scripts/cmake/packaging/PackagingLinux.cmake
+++ scripts/cmake/packaging/PackagingLinux.cmake
@@ -1,9 +1,6 @@
 set(CPACK_GENERATOR TGZ)
 # Adds the binaries location to the LD_LIBRARY_PATH
-set(CMAKE_INSTALL_RPATH \$ORIGIN/)
-if(OGS_USE_CONAN)
-    SET(CMAKE_INSTALL_RPATH "$ORIGIN/../lib")
-endif()
+SET(CMAKE_INSTALL_RPATH "$ORIGIN;$ORIGIN/../lib")
 
 if(MODULE_CMD)
     message(STATUS "Found module cmd -> writing module file.")
