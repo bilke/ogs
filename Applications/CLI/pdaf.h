ce95d9ff5c0a53f1d98eff27edcb7228714f6749
LSchueler <lennart.schueler@ufz.de>
2019-06-05 09:06:33 +0200

    PDAF header now points to C symbols

--- Applications/CLI/pdaf.h
+++ Applications/CLI/pdaf.h
@@ -1,6 +1,6 @@
-extern void c_init_parallel_pdaf(int* dim_ens, int* screen);
-extern void c_init_model(int* time_steps, double* data, int* x_no);
-extern void c_init_pdaf(int* ens_no);
-extern void c_assimilate_pdaf();
-extern void c_finalize_pdaf();
-extern int  c_get_rank();
+extern "C" void c_init_parallel_pdaf(int* dim_ens, int* screen);
+extern "C" void c_init_model(int* time_steps, double* data, int* x_no);
+extern "C" void c_init_pdaf(int* ens_no);
+extern "C" void c_assimilate_pdaf();
+extern "C" void c_finalize_pdaf();
+extern "C" int  c_get_rank();
